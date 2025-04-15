753ae7804c6d131840078a375794dc6f80042a8c
kosay batach <battachkozay@gmail.com>
2025-04-15 19:59:29 +0100

    second day

---
+++ script.js
@@ -0,0 +1,17 @@
+function Homee() {
+    document.querySelector(".right-side").style.display="block"
+    document.querySelector(".Aboutt").style.display="none";
+    document.querySelector(".Projectss").style.display="none";
+}
+
+function Aboutt(){
+    document.querySelector(".right-side").style.display="none"
+    document.querySelector(".Aboutt").style.display="block";
+    document.querySelector(".Projectss").style.display="none";
+}
+
+function Projectss(){
+    document.querySelector(".right-side").style.display="none"
+    document.querySelector(".Aboutt").style.display="none";
+    document.querySelector(".Projectss").style.display="block";
+}

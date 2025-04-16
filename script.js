7c24e8a77014ef448642040463e168cd993762b0
kosay batach <battachkozay@gmail.com>
2025-04-16 14:13:41 +0100

    third day

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ function Homee() {
     document.querySelector(".right-side").style.display="block"
     document.querySelector(".Aboutt").style.display="none";
     document.querySelector(".Projectss").style.display="none";
+    document.querySelector(".Skillss").style.display="none";
 }
 
 function Aboutt(){
     document.querySelector(".right-side").style.display="none"
     document.querySelector(".Aboutt").style.display="block";
     document.querySelector(".Projectss").style.display="none";
+    document.querySelector(".Skillss").style.display="none";
 }
 
 function Projectss(){
     document.querySelector(".right-side").style.display="none"
     document.querySelector(".Aboutt").style.display="none";
     document.querySelector(".Projectss").style.display="block";
+    document.querySelector(".Skillss").style.display="none";
+}
+function Skillss(){
+    document.querySelector(".right-side").style.display="none"
+    document.querySelector(".Aboutt").style.display="none";
+    document.querySelector(".Projectss").style.display="none";
+    document.querySelector(".Skillss").style.display="block";
 }
